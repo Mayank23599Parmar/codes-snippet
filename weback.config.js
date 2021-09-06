5d05fa0d5118e054bbcc5b270ad035f21b1a9d73
Mayank23599Parmar <79260831+Mayank23599Parmar@users.noreply.github.com>
2021-09-06 12:57:24 +0530

    Create weback.config.js

---
+++ weback.config.js
@@ -0,0 +1,101 @@
+var webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var config = {
+  module: {}
+};
+module.sorceEnable = false;
+
+const returnAll = watchFile => {
+
+  var checkoutPage = Object.assign({}, config, {
+    entry: ["@babel/polyfill", "./javascript/app-checkout.js"],
+    watch: watchFile,
+    output: {
+      path: __dirname,
+      filename: "theme/assets/theme-checkout.js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: ["@babel/plugin-proposal-class-properties"]
+            }
+          }
+        }
+      ]
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery"
+      })
+    ]
+  });
+  var allPages = Object.assign({}, config, {
+    entry: ["@babel/polyfill", "./javascript/app.js"],
+    watch: watchFile,
+    output: {
+      path: __dirname,
+      filename: "theme/assets/theme.js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                // you can specify a publicPath here
+                // by default it use publicPath in webpackOptions.output
+                publicPath: __dirname
+              }
+            },
+            "css-loader",
+            "sass-loader"
+          ]
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: ["@babel/plugin-proposal-class-properties"]
+            }
+          }
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: "theme/assets/theme.css"
+      }),
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery"
+      })
+    ]
+  });
+  return [
+    checkoutPage,
+    allPages,
+  ];
+}
+// Return Array of Configurations
+module.exports = (env, argv) => {
+  let watchFile = false;
+  if (argv.mode === "development") {
+    watchFile = true;
+  }
+  return returnAll(watchFile);
+
+};
