c3f1d55f322de67bf7e0dff46b14659fdf3366e4
Mayank Parmar <79260831+Mayank23599Parmar@users.noreply.github.com>
2024-06-26 16:23:42 +0530

    Update Shopify-graphql.js

--- Shopify-graphql.js
+++ Shopify-graphql.js
@@ -39,40 +39,132 @@ query MyQuery {
 
 
 // SEARCH FILTER START
-
-query MyQuery {
-  search(query: "Striped", first: 10, productFilters: [
-    {productMetafield: {namespace: "product", key: "topic", value: "Selection"}}
-  ]) {
+query Search($query: String!, $first: Int, $sortKey: SearchSortKeys, $types: [SearchType!], $productFilters: [ProductFilter!], $identifiers: [HasMetafieldsIdentifier!]!, $after: String, $variantsFirst2: Int) {
+  search(query: $query, first: $first, sortKey: $sortKey, types: $types, productFilters: $productFilters, after: $after) {
     totalCount
     productFilters {
+      id
+      label
+      presentation
       type
       values {
-        count
-        id
         label
         input
+        id
+        count
       }
-      label
-      id
-      presentation
     }
     pageInfo {
-      endCursor
-      hasNextPage
-      hasPreviousPage
       startCursor
+      hasPreviousPage
+      hasNextPage
+      endCursor
     }
     edges {
       cursor
       node {
         ... on Product {
-          id
+          vendor
+          totalInventory
           title
+          tags
+          handle
+          featuredImage {
+            altText
+            height
+            id
+            url
+            width
+          }
+          availableForSale
+          productType
+          compareAtPriceRange {
+            maxVariantPrice {
+              amount
+              currencyCode
+            }
+            minVariantPrice {
+              amount
+              currencyCode
+            }
+          }
+          metafields(identifiers: $identifiers) {
+            value
+            key
+            type
+            reference {
+              ... on Metaobject {
+                type
+                fields {
+                  key
+                  value
+                  reference {
+                    ... on GenericFile {
+                      url
+                      previewImage {
+                        width
+                        url
+                        altText
+                      }
+                      mimeType
+                      alt
+                    }
+                  }
+                }
+              }
+            }
+          }
+          variants(first: $variantsFirst2) {
+            nodes {
+              id
+              title
+              sku
+              price {
+                amount
+                currencyCode
+              }
+              compareAtPrice {
+                amount
+                currencyCode
+              }
+              availableForSale
+            }
+          }
         }
       }
     }
   }
 }
+ const variables = {
+                query: searchValue,
+                first: 9,
+                sortKey: "RELEVANCE",
+                types: "PRODUCT",
+                productFilters: [
+                    {
+                        tag: searchCategory
+                    },
+                    {
+                        productMetafield:{
+                                namespace: "product",
+                                key: "topic_list",
+                                value: "Selection"
+                            }
+                        
+                    }
+                ],
+                identifiers: [
+                    {
+                        key: "expert_details",
+                        namespace: "product"
+                    },
+                    {
+                        key: "course_start_at",
+                        namespace: "product"
+                    }
+                ],
+                after: endCursor,
+                variantsFirst2: 4 
+            }
 // SEARCH FILTER END
 //==============================================================================================
