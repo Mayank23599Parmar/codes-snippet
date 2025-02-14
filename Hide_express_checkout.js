cb5fe477fec6c8a4ad1d93ca222cba5064bdb91a
Mayank Parmar <79260831+Mayank23599Parmar@users.noreply.github.com>
2025-02-14 15:35:17 +0530

    Create Hide_express_checkout.js

---
+++ Hide_express_checkout.js
@@ -0,0 +1,52 @@
+// @ts-check
+
+/**
+ * @typedef {import("../generated/api").RunInput} RunInput
+ * @typedef {import("../generated/api").FunctionRunResult} FunctionRunResult
+ */
+
+/**
+ * @type {FunctionRunResult}
+ */
+const NO_CHANGES = {
+  operations: [],
+};
+
+/**
+ * @param {RunInput} input
+ * @returns {FunctionRunResult}
+ */
+export function run(input) {
+  const  lines= input?.cart?.lines
+  const excludedMethods = new Set(["Apple Pay", "Google Pay", "Shop Pay", "Shop Pay Installments","Paypal Pay","Paypal"])
+  const hidePaymentMethods=[]
+  const paymentMethods=input?.paymentMethods
+  let checkLineItemshasGiftCard=false;
+  if(lines?.length > 0){
+    for (let i = 0; i < lines.length; i++) {
+       const line=  lines[i];
+       const productType = line?.merchandise?.product?.productType
+       if(productType == "Gift Card" || productType == "Spa Day"  ){
+         checkLineItemshasGiftCard=true
+       }
+  }
+  }  
+  if(checkLineItemshasGiftCard && paymentMethods?.length > 0){
+    for (var i = 0; i < paymentMethods.length; i++) {
+      const method= paymentMethods[i]
+      if( excludedMethods.has(method?.name)){
+        hidePaymentMethods.push(    {
+        hide: {
+          paymentMethodId: method?.id
+        }
+      })
+      }
+    }
+  }
+  if(hidePaymentMethods?.length > 0){
+    return {
+      operations:hidePaymentMethods
+    }
+  }
+  return NO_CHANGES
+}
