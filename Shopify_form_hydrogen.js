bdd223719eadfa2e258d0c6ba1ebfefdcfb5306a
Mayank Parmar <79260831+Mayank23599Parmar@users.noreply.github.com>
2025-11-11 20:31:08 +0530

    Implement Content Security Policy in handleRequest
    Add Content Security Policy configuration for Shopify forms.

--- Shopify_form_hydrogen.js
+++ Shopify_form_hydrogen.js
@@ -138,3 +138,77 @@ console.log(scriptLoadStatus,"ssssssss");
   );
 };
 export default ShopifyForm;
+
+
+// ================================================================
+//  go to entry.server.jsx and add below routes In handleRequest function
+
+export default async function handleRequest(
+  request,
+  responseStatusCode,
+  responseHeaders,
+  reactRouterContext,
+  context,
+) {
+  const {nonce, header, NonceProvider} = createContentSecurityPolicy({
+    shop: {
+      checkoutDomain: context.env.PUBLIC_CHECKOUT_DOMAIN,
+      storeDomain: context.env.PUBLIC_STORE_DOMAIN,
+    },
+
+    defaultSrc: [
+      'self',
+      'https://cdn.shopify.com',
+      'https://shopify.com',
+       'https://js.hcaptcha.com '
+    ],
+    // Allow YouTube iframes
+    frameSrc: [
+      "'self'",
+      'https://www.youtube.com',
+      'https://youtube.com',
+      'https://*.youtube.com',
+      'https://*.youtube-nocookie.com',
+      'https://js.hcaptcha.com',
+      'https://fonts.shopifycdn.com',
+      'https://newassets.hcaptcha.com'
+    ],
+    connectSrc: [
+      'self',
+      'https://forms.shopifyapps.com',
+      'https://otlp-http-production.shopifysvc.com',
+      'https://notify.bugsnag.com',
+    ]
+  });
+
+  const body = await renderToReadableStream(
+    <NonceProvider>
+      <ServerRouter
+        context={reactRouterContext}
+        url={request.url}
+        nonce={nonce}
+      />
+    </NonceProvider>,
+    {
+      nonce,
+      signal: request.signal,
+      onError(error) {
+        console.error(error);
+        responseStatusCode = 500;
+      },
+    },
+  );
+
+  if (isbot(request.headers.get('user-agent'))) {
+    await body.allReady;
+  }
+
+  responseHeaders.set('Content-Type', 'text/html');
+  responseHeaders.set('Content-Security-Policy', header);
+
+  return new Response(body, {
+    headers: responseHeaders,
+    status: responseStatusCode,
+  });
+}
+
